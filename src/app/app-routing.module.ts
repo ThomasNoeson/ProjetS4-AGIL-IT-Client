852d71cee70317152505d2b8fb8a370a322fc19a
thomas.noeson <thomas.noeson@info1109.iut-lens.univ-artois.fr>
2021-03-31 11:54:09 +0200

    avancement issue5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import {LpSolverTestComponent} from './lp-solver-test/lp-solver-test.component';
 import {AcceuilComponent} from './acceuil/acceuil.component';
 import {JeuxComponent} from './jeux/jeux.component';
 import {AjoutUtilisateurComponent} from './ajout-utilisateur/ajout-utilisateur.component';
+import {AjoutJeuxComponent} from './ajout-jeux/ajout-jeux.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'jeux', pathMatch: 'full'},
   {path: 'jeux', component: JeuxComponent},
   {path: 'create', component: AjoutUtilisateurComponent},
+  {path: 'ajout_jeux', component: AjoutJeuxComponent},
   {path: 'logout', component: AcceuilComponent},
   {path: 'login', component: LoginComponent},
   {path: 'profile', component: ProfileComponent},
