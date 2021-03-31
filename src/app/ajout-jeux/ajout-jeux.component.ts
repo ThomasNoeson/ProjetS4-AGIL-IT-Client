d9eaade6f46fbede619cf23e327690b18de37629
thomas.noeson <thomas_noeson@ent.univ-artois.fr>
2021-03-31 13:48:19 +0200

    parti ajout d'un jeu

--- src/app/ajout-jeux/ajout-jeux.component.ts
+++ src/app/ajout-jeux/ajout-jeux.component.ts
@@ -8,16 +8,20 @@ import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/form
 })
 
 export class AjoutJeuxComponent implements OnInit {
+  loading: boolean;
   formulaire = new FormGroup({
     nom: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(100)]),
     nombrejoueurs: new FormControl('', [Validators.required, Validators.pattern('^[2-8]*$'), Validators.maxLength(1)]),
-    ageminimum : new FormControl('', [Validators.required, Validators.pattern('^\\d$|^1[0-6]$'), Validators.maxLength(2)]),
-    poids : new FormControl('', [Validators.required, Validators.pattern('^[0-4.9]+\\d{1,2}(?:\\d)?$'), Validators.maxLength(5)]),
+    ageminimum: new FormControl('', [Validators.required, Validators.pattern('^\\d$|^1[0-6]$'), Validators.maxLength(2)]),
+    poids: new FormControl('', [Validators.required, Validators.min(0.100), Validators.max(5)]),
   });
 
-  constructor() { }
+  constructor() {
+    this.loading = false;
+  }
 
   ngOnInit(): void {
+    this.loading = true;
   }
 
   get nom(): AbstractControl {
@@ -32,4 +36,11 @@ export class AjoutJeuxComponent implements OnInit {
     return this.formulaire.get('nombrejoueurs');
   }
 
+  get ageminimum(): AbstractControl {
+    return this.formulaire.get('ageminimum');
+  }
+
+  get poids(): AbstractControl {
+    return this.formulaire.get('poids');
+  }
 }
